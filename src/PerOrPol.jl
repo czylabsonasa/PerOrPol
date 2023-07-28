5b26087650f8ca3a1788703501df823e0f216a74
czylabsonasa <czylabson.asa@gmail.com>
2023-07-28 22:40:04 +0200

    Newton3

--- src/PerOrPol.jl
+++ src/PerOrPol.jl
@@ -10,12 +10,17 @@ module PerOrPol
   const _co_lo=-30
   const _co_up=30
 
-"""  
-  it generates a random quadratic poly with Newton period of length 2 
-  returns (pol=(a,b,c),x=(x1,x2)), where the required polynomial is ax^2+bx+c, and 
-  Newton's method generates: x1->x2->x1...
+"""
+    Newton2(;<keyword arguments>)
+  it generates a random quadratic poly with Newton-period of length 2 and
+  returns a namedtuple `(pol=(a,b,c),x=(x1,x2))`, where the required polynomial is ax^2+bx+c and 
+  Newton's method generates: `x1->x2->x1...`.
+
+# Arguments:
+* they are lower and upper bounds related to the random selection of the numbers used in the process
 """
   function Newton2(
+    ;
     pool_lo::Int=_pool_lo,
     pool_up::Int=_pool_up,
     co_lo::Int=_co_lo,
@@ -32,6 +37,8 @@ module PerOrPol
     x1,x2,a,b,c=fill(0//1,5)
     while true
       x1,x2=sample(pool, 2, replace=false)
+      # based on the condition one can set up a 2x2 linear system w/ unknowns a,b and 
+      # [c,c]' rigth hand side - the coeff matrix is:
       LHS=[x1*(x1-2*x2) -x2; x2*(x2-2*x1) -x1]
       iszero(det(LHS)) && continue
       c=rand(pool)
@@ -39,8 +46,47 @@ module PerOrPol
       a,b=LHS\RHS
       all(co_lo .≤ [a.num,a.den,b.num,b.den] .≤ co_up) && break
     end
-    (pol=(a,b,c),orbity=(x1,x2))
+    (pol=(a,b,c),orbit=(x1,x2))
   end
+
+"""  
+    Newton3(;<keyword arguments>)
+
+  it generates a random quadratic poly with Newton-period of length 3 and
+  returns a namedtuple `(pol=(a,b,c,d),x=(x1,x2,x3))`, where the required polynomial is `ax^3+bx^2+cx+d`, and 
+  Newton's method generates the orbit: `x1->x2->x3->x1...`
+
+# Arguments:
+* they are lower and upper bounds related to the random selection of the numbers used in the process
+"""
+  function Newton3(
+    pool_lo::Int=_pool_lo,
+    pool_up::Int=_pool_up,
+    co_lo::Int=_co_lo,
+    co_up::Int=_co_up
+  )
+    
+    alap=collect(pool_lo:pool_up)
+    pool=Rational{Int}[]
+    for den in [1,2,4,8]
+      pool=vcat(pool,alap//den)
+    end
+    pool=setdiff(pool, 0) # all coeff and the orbit points are nonzero (ad-hoc condition)
+
+    genrow(x,y)=[2*x^3-3*x^2*y, x^2-2*x*y, -y]'
+    x1,x2,x3,a,b,c,d=fill(0//1,7)
+    while true
+      x1,x2,x3=sample(pool, 3, replace=false)
+      LHS=vcat(genrow(x1,x2),genrow(x2,x3),genrow(x3,x1))
+      iszero(det(LHS)) && continue
+      d=rand(pool)
+      RHS=[d; d; d]
+      a,b,c=LHS\RHS
+      all(co_lo .≤ [a.num,a.den,b.num,b.den,c.num,c.den] .≤ co_up) && break
+    end
+    (pol=(a,b,c,d),orbit=(x1,x2,x3))
+  end
+  
   
 
 end # module PerOrPol
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,13 +5,12 @@ import Test:
 import Polynomials: 
   Polynomial,derivative
 import PerOrPol: 
-  Newton2
+  Newton2, Newton3
 
-printstyled(stderr,"Newton\n",color=:light_yellow)
 # Newton step
 step(x,f,df)=x-f(x)//df(x)
 
-@testset begin 
+@testset "Newton2" begin 
   for t in 1:10
     ret=Newton2()
     p=Polynomial(ret.pol[3:-1:1]) # Polynomial expects coeffs in increasing order
@@ -20,3 +19,15 @@ step(x,f,df)=x-f(x)//df(x)
     @test step(x1,p,dp)==x2 && step(x2,p,dp)==x1
   end 
 end
+
+
+@testset "Newton3" begin 
+  for t in 1:10
+    ret=Newton3()
+    p=Polynomial(ret.pol[4:-1:1]) # Polynomial expects coeffs in increasing order
+    dp=derivative(p)
+    x1,x2,x3=ret.orbit
+    @test step(x1,p,dp)==x2 && step(x2,p,dp)==x3 && step(x3,p,dp)==x1
+  end 
+end
+
